fd5d3cd7b449995a93db5f0757a767b931da1161
Fabian Andres <velozafabian25@gmail.com>
2026-09-06 19:15:44 -0500

    punto 17

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -210,11 +210,13 @@ public class Workshop {
         return rotado;
  }
 
+    // Punto 17
     // Método que cuenta los caracteres en una cadena
     public int contarCaracteres(String cadena) {
         // TODO: Implementar el método para contar el número de caracteres en una cadena.
         // Ejemplo: Si cadena = "Hello", el resultado debería ser 5.
-        return 0;
+        if (cadena == null) return 0;
+        return cadena.length();
     }
 
     // Método que invierte una cadena
