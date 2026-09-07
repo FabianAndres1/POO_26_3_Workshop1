4df6913698b579b410aee40521c4a63a9d0121c7
Fabian Andres <velozafabian25@gmail.com>
2026-09-06 19:38:30 -0500

    a

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -210,9 +210,21 @@ public class Workshop {
         return rotado;
  }
 
-// Punto 17
-    public int contarCaracteres(String cadena) {
-        return cadena.length();
+ // Punto 16
+    // Método que rota un arreglo n posiciones
+    public int[] rotarArreglo(int[] arreglo, int posiciones) {
+        // TODO: Implementar el método para rotar un arreglo n posiciones.
+        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5] y posiciones = 2, el resultado debería ser [3, 4, 5, 1, 2].
+        int n = arreglo.length;
+        if (n == 0) return new int[0];
+        posiciones = posiciones % n;
+        if (posiciones < 0) posiciones += n;
+
+        int[] rotado = new int[n];
+        for (int i = 0; i < n; i++) {
+            rotado[(i + posiciones) % n] = arreglo[i];
+        }
+        return rotado;
     }
 
     // Punto 18
